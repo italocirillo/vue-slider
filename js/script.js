c3db0c8c5362ed83bd395aef7f235bf73bd3bf1c
italocirillo <italocirillo@gmail.com>
2023-03-29 22:56:31 +0200

    ESERCIZIO COMPLETO + BONUS

--- js/script.js
+++ js/script.js
@@ -1,67 +1,66 @@
-
 const { createApp } = Vue;
 
 createApp({
-    data(){
-        return{
+    data() {
+        return {
             immagineAttiva: 0,
-            giochi : 
-            [
-                        {
-                            image: 'img/01.webp',
-                            title: 'Marvel\'s Spiderman Miles Morale',
-                            text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-                        }, {
-                            image: 'img/02.webp',
-                            title: 'Ratchet & Clank: Rift Apart',
-                            text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-                        }, {
-                            image: 'img/03.webp',
-                            title: 'Fortnite',
-                            text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-                        }, {
-                            image: 'img/04.webp',
-                            title: 'Stray',
-                            text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-                        }, {
-                            image: 'img/05.webp',
-                            title: "Marvel's Avengers",
-                            text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-                        }
-            ],
-            millisecondi : 3000,
-            intervallo : null,
+            giochi:
+                [
+                    {
+                        image: 'img/01.webp',
+                        title: 'Marvel\'s Spiderman Miles Morale',
+                        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                    }, {
+                        image: 'img/02.webp',
+                        title: 'Ratchet & Clank: Rift Apart',
+                        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                    }, {
+                        image: 'img/03.webp',
+                        title: 'Fortnite',
+                        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                    }, {
+                        image: 'img/04.webp',
+                        title: 'Stray',
+                        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                    }, {
+                        image: 'img/05.webp',
+                        title: "Marvel's Avengers",
+                        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                    }
+                ],
+            millisecondi: 3000,
+            intervallo: null,
         }
     },
-    mounted(){
+    mounted() {
         this.autoPlay();
     },
     methods: {
-        immagineSuccessiva(){
-            if (this.immagineAttiva < this.giochi.length -1){
-                this.immagineAttiva ++;
-            }else{
+        immagineSuccessiva() {
+            if (this.immagineAttiva < this.giochi.length - 1) {
+                this.immagineAttiva++;
+            } else {
                 this.immagineAttiva = 0;
             }
             // Intervallo
             clearInterval(this.intervallo);
             this.intervallo = setInterval(this.immagineSuccessiva, this.millisecondi);
         },
-        immaginePrecedente(){
-            if (this.immagineAttiva > 0){
-                this.immagineAttiva --;
-            }else{
-                this.immagineAttiva = this.giochi.length -1;
+        immaginePrecedente() {
+            if (this.immagineAttiva > 0) {
+                this.immagineAttiva--;
+            } else {
+                this.immagineAttiva = this.giochi.length - 1;
             }
             // Intervallo
             clearInterval(this.intervallo);
             this.intervallo = setInterval(this.immaginePrecedente, this.millisecondi);
         },
-        autoPlay(){
+        autoPlay() {
             clearInterval(this.intervallo);
             this.intervallo = setInterval(this.immagineSuccessiva, this.millisecondi);
         },
-        stopInterval(){
+        stopInterval() {
             clearInterval(this.intervallo);
         }
     }
